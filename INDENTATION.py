fda45be869d8e32f66dca53a0d040e24cbd0ac7c
Amaranatha V <117143794+amarv4590@users.noreply.github.com>
2023-03-25 21:32:50 +0530

    Indentation to indicate a block of code

---
+++ INDENTATION.py
@@ -0,0 +1,7 @@
+#Indentation refers to the spaces at the beginning of a code line.
+#Python uses indentation to indicate a block of code.
+#The number of spaces is up to you as a programmer, but it has to be at least one.
+#You have to use the same number of spaces in the same block of code.
+#Example
+if 2>1:
+    print("Two is greater than One")
