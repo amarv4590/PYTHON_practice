9280de29be70c161081ae60efb219a782ff97ee1
Amaranatha V <117143794+amarv4590@users.noreply.github.com>
2023-03-25 21:25:51 +0530

    Delete SYNTAX.py

--- SYNTAX.py
+++
@@ -1,6 +0,0 @@
-#Python syntax can be executed by writing directly in the Command Line
->>> print("Hello, World!")
-Hello, World!
-
-#Or by creating a python file on the server, using the .py file extension, and running it in the Command Line
-C:\Users\Your Name>python myfile.py 
